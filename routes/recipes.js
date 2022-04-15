528b4f4b0ba69df9caf8fe00e9b4ce07fa1bd3fa
sberger94 <sberger94@gmail.com>
2022-04-15 00:23:51 -0700

    Updated styling

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,9 +4,9 @@ const recipesController = require('../controllers/recipes');
 const isLoggedIn = require('../config/auth');
 
 router.get('/', recipesController.index);
-router.get('/new', recipesController.new);
+router.get('/new', isLoggedIn,recipesController.new);
 router.get('/:id', recipesController.show);
 router.post('/', recipesController.create);
-router.delete('/:id', recipesController.delete);
+router.delete('/:id', isLoggedIn, recipesController.delete);
 
 module.exports = router;
